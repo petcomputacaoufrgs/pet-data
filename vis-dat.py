c06028f068e87da0f5c6d21ce19e925beab1454a
birromer <hummescmpa@gmail.com>
2018-07-17 11:52:29 -0300

    udpated vis

--- vis-dat.py
+++ vis-dat.py
@@ -1,23 +1,12 @@
 import numpy as np
 import pandas as pd
-import math
-
 
 df = pd.read_csv('AvalDiscente_20xx-x.csv', sep=';', error_bad_lines=False)
 
-#for item in df['Q1']:
-#    print(str(type(item)) + ' ' +  str(item))
+header = list(df.columns.values)
 
-df['Q1'][df['Q1'].isnull()] = None
-df['Q2'][df['Q2'].isnull()] = None
-df['Q3'][df['Q3'].isnull()] = None
-df['Q4'][df['Q4'].isnull()] = None
-df['Q5'][df['Q5'].isnull()] = None
-df['Q6'][df['Q6'].isnull()] = None
-df['Q7'][df['Q7'].isnull()] = None
-df['Q8'][df['Q8'].isnull()] = None
-df['Q9'][df['Q9'].isnull()] = None
-df['Q10'][df['Q10'].isnull()] = None
-df['Q11'][df['Q11'].isnull()] = None
+for h in header[2:-1]:
+    df[h][df[h].isnull()] = None
 
-print(df)
+if __name__ == "__main__":
+    print(df)
