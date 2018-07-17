272a5327f4791cf855f889d8e14c7c403a12b9a7
birromer <hummescmpa@gmail.com>
2018-07-17 11:55:01 -0300

    udpated vis

--- vis-dat.py
+++ vis-dat.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 df = pd.read_csv('AvalDiscente_20xx-x.csv', sep=';', error_bad_lines=False)
 
-header = list(df.columns.values)
+header = list(df.columns.values) #list with column names
 
 for h in header[2:-1]:
+    #replaces NaN values with None
     df[h][df[h].isnull()] = None
 
 if __name__ == "__main__":
